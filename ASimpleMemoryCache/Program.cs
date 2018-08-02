6d407b39ea44e7ec0c6701caed2f1ccaab9fb927
Jason Qu <fishinsea2014@gmail.com>
2018-08-02 15:45:17 +1200

    Complete a basic cache.

---
+++ ASimpleMemoryCache/Program.cs
@@ -0,0 +1,47 @@
+﻿using Jason.common;
+using Jason.Dll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASimpleMemoryCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine($"Obtain {nameof(DBHelper)} for {i} times {DateTime.Now.ToString("yyyyMMdd HHmmss.fff")}" );
+                        List<Program> programList = null;
+                        string key = $"{nameof(DBHelper)}_Query_{nameof(Program)}_{123}";
+
+                        if (!CustomCache.Exist(key))
+                        {
+                            programList = DBHelper.Query<Program>(123);
+                            CustomCache.Add(key, programList);
+                        }
+                        else
+                        {
+                            programList = CustomCache.Get<List<Program>>(key);
+                        }
+
+                        //
+                        Console.WriteLine($"The {i} time you fetch the data is {programList.GetHashCode()}");
+                    }
+                }
+
+                Console.Read();
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Jason.common/CustomCache.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jason.common
+{
+    public class CustomCache
+    {
+        private static Dictionary<string, object> CustomCacheDictionary;// Static ensure global unique and donot release.
+
+        static CustomCache()
+        {
+            CustomCacheDictionary = new Dictionary<string, object>();
+            Console.WriteLine($"{DateTime.Now.ToString("MM-dd HH:mm:ss fff")} initiate the cache"); //Record when the system restart
+
+        }
+
+        public static void Add (string key, object value)
+        {
+            CustomCacheDictionary.Add(key, value);
+        }
+
+        /// <summary>
+        /// If the item exist in dict, then recover it, or add a new one.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SaveOrUpdate(string key, object value)
+        {
+            CustomCacheDictionary[key] = value;
+        }
+
+        public static T Get <T>(string key)
+        {
+            return (T)CustomCacheDictionary[key];
+        }
+
+        public static bool Exist(string key)
+        {
+            return CustomCacheDictionary.ContainsKey(key);
+        }
+
+    }
+}
